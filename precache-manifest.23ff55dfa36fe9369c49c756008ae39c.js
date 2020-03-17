c9542163d5ec364c400664f2db93dc4b0d4d3ec3
kamomkoyan2 <kamkoyan@gmail.com>
2020-03-17 10:31:34 +0400

    Updates

---
+++ precache-manifest.23ff55dfa36fe9369c49c756008ae39c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0106e6d1a16ef909819870fd100814c",
+    "url": "/kamomkoyan2/robofriends/index.html"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/kamomkoyan2/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "d1eeb1b03a421767207f",
+    "url": "/kamomkoyan2/robofriends/static/css/main.caccaeb4.chunk.css"
+  },
+  {
+    "revision": "978d3e6abce16fc74e6c",
+    "url": "/kamomkoyan2/robofriends/static/js/2.faf57d51.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/kamomkoyan2/robofriends/static/js/2.faf57d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1eeb1b03a421767207f",
+    "url": "/kamomkoyan2/robofriends/static/js/main.3e82d192.chunk.js"
+  },
+  {
+    "revision": "311228c2a290aa7bdbeb",
+    "url": "/kamomkoyan2/robofriends/static/js/runtime-main.460be5b5.js"
+  }
+]);
